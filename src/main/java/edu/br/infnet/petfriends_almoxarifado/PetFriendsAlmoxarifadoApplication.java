a45b09347eafa21ae24c3d2d4a804478302b721b
Mariana Buhrer <mah_buhrer@hotmail.com>
2024-09-22 19:48:43 -0300

    adicionando melhorias no ms

--- src/main/java/edu/br/infnet/petfriends_almoxarifado/PetFriendsAlmoxarifadoApplication.java
+++ src/main/java/edu/br/infnet/petfriends_almoxarifado/PetFriendsAlmoxarifadoApplication.java
@@ -9,5 +9,4 @@ public class PetFriendsAlmoxarifadoApplication {
     public static void main(String[] args) {
         SpringApplication.run(PetFriendsAlmoxarifadoApplication.class, args);
     }
-
 }
